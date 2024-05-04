d3f2714e5a08af52b54c25f75c46875067ec624c
oumaima <oumaima>
2024-05-04 17:17:26 +0100

    liste

---
+++ liste.js
@@ -0,0 +1,26 @@
+const mathcs = document.getElementById("maths")
+const englishh = document.getElementById("english")
+const physics = document.getElementById("physics")
+
+function displaysubject(){
+    
+        maths.style.display ="block";
+        
+        english.style.display ="block";
+          
+          
+       physics.style.display ="block";
+          
+
+    }
+    function displaysubjects(){
+    
+        maths.style.display ="none";
+        
+        english.style.display ="none";
+          
+          
+       physics.style.display ="none";
+          
+
+    }
